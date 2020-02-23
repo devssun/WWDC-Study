ed99341eed1cec7bb4942756d51189c98b67bf16
devssun <chocochic85@gmail.com>
2020-02-23 23:42:20 +0900

    초기셋팅

---
+++ STTExample/STTExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STTExample
+//
+//  Created by 최혜선 on 2020/02/23.
+//  Copyright © 2020 jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
