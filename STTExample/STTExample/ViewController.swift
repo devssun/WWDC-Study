c4856393dafd86b84ba15cc59845943a071c06c9
devssun <chocochic85@gmail.com>
2020-02-24 12:28:01 +0900

    음성 인식 기능 추가
    https://developer.apple.com/documentation/speech/recognizing_speech_in_live_audio#overview

--- STTExample/STTExample/ViewController.swift
+++ STTExample/STTExample/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     
     @IBOutlet fileprivate weak var textView: UITextView!
     @IBOutlet fileprivate weak var recordingButton: UIButton!
+    
+    private let audioEngine = AVAudioEngine()
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private var speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko-KR"))!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +27,19 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        speechRecognizer.delegate = self
+        
         SFSpeechRecognizer.requestAuthorization { (status) in
             OperationQueue.main.addOperation {
                 switch status {
                 case .authorized:
                     print("허용")
+                    self.recordingButton.isEnabled = true
+                    self.recordingButton.setTitle("Start Recording", for: [])
                 case .denied, .notDetermined, .restricted:
                     print("거부")
+                    self.recordingButton.isEnabled = false
+                    self.recordingButton.setTitle("unavailable Recording", for: [])
                 @unknown default:
                     fatalError("권한 요청 실패")
                 }
@@ -36,8 +47,78 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction private func touchedRecordingButton(_ sender: UIButton) {
+    private func startRecording() throws {
+        recognitionTask?.cancel()
+        recognitionTask = nil
         
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        let inputNode = audioEngine.inputNode
+        
+        let recordingFormmat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormmat) { (buffer, time) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object")
+        }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal: Bool = false
+            
+            if let result = result {
+                self.textView.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+                print("result \(result.bestTranscription.formattedString)")
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                self.recordingButton.isEnabled = true
+                self.recordingButton.setTitle("Start Recording", for: [])
+            }
+        })
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        textView.text = "Tell Something"
+    }
+    
+    @IBAction private func touchedRecordingButton(_ sender: UIButton) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordingButton.isEnabled = false
+            recordingButton.setTitle("Stopping", for: .normal)
+        } else {
+            do {
+                recordingButton.isEnabled = true
+                recordingButton.setTitle("Stop Recording", for: .normal)
+                try startRecording()
+            } catch {
+                self.recordingButton.isEnabled = false
+                self.recordingButton.setTitle("error", for: .normal)
+            }
+        }
     }
 }
 
+extension ViewController: SFSpeechRecognizerDelegate {
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            self.recordingButton.isEnabled = true
+            self.recordingButton.setTitle("Start Recording", for: [])
+        } else {
+            self.recordingButton.isEnabled = false
+            self.recordingButton.setTitle("unavailble", for: .disabled)
+        }
+    }
+}
