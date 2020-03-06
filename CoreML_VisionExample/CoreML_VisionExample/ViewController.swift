49c66af17dee2f145fa204bca2f0c96dba4f64fd
devssun <chocochic85@gmail.com>
2020-03-06 15:28:43 +0900

    CoreML + Vision 프로젝트 초기 커밋

---
+++ CoreML_VisionExample/CoreML_VisionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML_VisionExample
+//
+//  Created by 최혜선 on 2020/03/06.
+//  Copyright © 2020 jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
