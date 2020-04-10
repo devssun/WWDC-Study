3fed46524216ffa292d6e343f4e64815c5849411
devssun <chocochic85@gmail.com>
2020-04-10 17:13:22 +0900

    Universal Links 테스트 앱 2 프로젝트 추가

---
+++ UniversalLinks2/UniversalLinks2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalLinks2
+//
+//  Created by 최혜선 on 2020/04/10.
+//  Copyright © 2020 jamie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
